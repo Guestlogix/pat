0d7424a0c362aff317ce3a865fff8b46e68b0f53
Hanker <hanker@Sarahs-MacBook-Air-2.local>
2019-10-08 18:12:48 -0400

    chore: check if env vars are available

--- actions/auto-version.sh
+++ actions/auto-version.sh
@@ -5,10 +5,6 @@ echo "Current Version: $CURRENT_VERSION"
 echo "New Version: $NEW_VERSION"
 if [ "$CURRENT_VERSION" != "$NEW_VERSION" ]; then
     echo "Updating commit with new semver tag."
-    # get repo name from git
-    remote=$(git config --get remote.origin.url)
-    repo=$(basename $remote .git)
-
     # POST a new ref to repo via Github API
     curl -X POST https://api.github.com/repos/$GITHUB_REPOSITORY/git/refs -H 'Authorization: token $GITHUB_ACCESS_TOKEN' -d '{"ref": "refs/tags/$NEW_VERSION","sha": "$GITHUB_SHA"}'
 else
--- actions/entry.sh
+++ actions/entry.sh
@@ -2,6 +2,7 @@
 # Add the path to the profile running this sh
 PATH=$PATH:/bin:/usr/bin:/go/bin
 export PATH
+printenv
 # Run the desired action script
 echo "Running $1 Pipeline Script"
 /tmp/$1.sh
