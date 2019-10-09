90081da2327b6ad9b229a5f4ff3df39a25145695
Hanker <hanker@macbookair-4e6a.glx.local>
2019-10-08 20:16:34 -0400

    chore: more comments

--- actions/entry.sh
+++ actions/entry.sh
@@ -2,6 +2,7 @@
 # Add the path to the profile running this sh
 PATH=$PATH:/bin:/usr/bin:/go/bin
 export PATH
+# Check out the environment variables (for debugging)
 printenv
 # Run the desired action script
 echo "Running $1 Pipeline Script"
